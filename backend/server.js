e711390387f72a9a85c6ae2fda8cfd5a0bc66ce6
tiochencho <yaelrubio12@aragon.unam.mx>
2026-05-21 00:56:09 -0600

    Fix

--- backend/server.js
+++ backend/server.js
@@ -95,7 +95,7 @@ app.delete('/cotizacion/:id', async (req, res) => {
 
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`)
